b11641def5aca6a198d0665d341bc2a13b607539
abductedRhino <avocadorable@mailbox.org>
2022-12-12 17:12:37 +0100

    ohje

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,17 +1,161 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class ChessBoard {
-    private final int rows;
+    private final int size;
     private ArrayList<Integer[][]> solutions;
-    private final int[][] board;
+    public final int[][] board;
 
-    public ChessBoard(int rows) {
-        this.rows = rows;
-        this.board = new int[rows][rows];
+    public ChessBoard(int size) {
+        this.size = size;
+        this.board = new int[size][size];
     }
 
     public void run() {
+        clearBoard();
         this.solutions = new ArrayList<>();
-
+        backtrack(0, 0);
     }
+
+    private void clearBoard() {
+        for (int[] row : board) {
+            Arrays.fill(row, 0);
+        }
+    }
+
+    public void backtrack(int row, int column) {
+        System.out.println(this);
+        boolean danger = board[row][column] > 0;
+        // no save spot in this row:
+        if (column == size - 1 && danger) {
+            // if last queen not on edge, move it.
+            if (findQueen(row - 1) != size - 1) {
+                backtrack(row - 1, removeQueen(row - 1) + 1);
+            }
+            // if last queen on edge and last row > 0
+            else if (row - 1 > 0) {
+                removeQueen(row - 1);
+                backtrack(row - 2, removeQueen(row - 2) + 1);
+            }
+            // if last queen on edge and last row == 0
+            else if (row - 1 == 0) {
+                System.out.println("done.");
+            }
+        }
+        // this spot is unsafe, but more columns are available in the same row:
+        else if (danger) {
+            backtrack(row, column + 1);
+        }
+        // this spot is safe, and it is the last row:
+        else if (row == size - 1) {
+            addQueen(row, column);
+            System.out.println(this.toString());
+        }
+        // this spot is safe, but not last row:
+        else {
+            addQueen(row, column);
+            backtrack(row + 1, 0);
+        }
+    }
+
+    public int removeQueen(int row) {
+        int column = findQueen(row);
+        // set queen to empty:
+        board[row][column] = 0;
+        // remove her threats
+        int i = 1;
+        while (row - i >= 0 || row + i < size) {
+            // can go up?
+            if (row - i >= 0) {
+                board[row - i][column]--;
+                //can go left?
+                if (column - i >= 0) {
+                    board[row - i][column - i]--;
+                }
+                //can go right?
+                if (column + i < size) {
+                    board[row - i][column + i]--;
+                }
+            }
+            //can go down?
+            if (row + i < size) {
+                board[row + i][column]--;
+                //can go left?
+                if (column - i >= 0) {
+                    board[row + i][column - i]--;
+                }
+                //can go right?
+                if (column + i < size) {
+                    board[row + i][column + i]--;
+                }
+            }
+            i++;
+        }
+        return column;
+    }
+
+    public int findQueen(int row) {
+        for (int column = 0; column < size; column++) {
+            if (board[row][column] == -1) {
+                return column;
+            }
+        }
+        return 0;
+    }
+
+    public void addQueen(int row, int column) {
+        board[row][column] = -1;
+        int i = 1;
+        while (row - i >= 0 || row + i < size) {
+            // can go up?
+            if (row - i >= 0) {
+                board[row - i][column]++;
+                //can go left?
+                if (column - i >= 0) {
+                    board[row - i][column - i]++;
+                }
+                //can go right?
+                if (column + i < size - 1) {
+                    board[row - i][column + i]++;
+                }
+            }
+            //can go down?
+            if (row + i < size) {
+                board[row + i][column]++;
+                //can go left?
+                if (column - i >= 0) {
+                    board[row + i][column - i]++;
+                }
+                //can go right?
+                if (column + i < size) {
+                    board[row + i][column + i]++;
+                }
+            }
+            i++;
+        }
+    }
+
+    private boolean threatened(int row, int column) {
+        Random r = new Random();
+        return r.nextBoolean();
+    }
+
+    @Override
+    public String toString() {
+        String ret = "";
+        ret = ret.concat("\n");
+        for (int row = 0; row < board.length; row++) {
+            for (int column = 0; column < board[row].length; column++) {
+                if (board[row][column] == -1) {
+                    ret = ret.concat("|Q");
+                } else if (board[row][column] > -1) {
+                    ret = ret.concat("|_");
+                }
+            }
+            ret = ret.concat("|\n");
+        }
+        return ret;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         QueenViewer viewer = new QueenViewer(1000);
         viewer.makeFrame();
+        ChessBoard board = new ChessBoard(4);
+        System.out.println(board.toString());
+        board.addQueen(0, 1);
+        board.addQueen(1, 3);
+        board.addQueen(2, 0);
+        board.addQueen(3, 2);
+        System.out.println(board.toString());
+        System.out.println(Arrays.toString(board.board[0]));
+        System.out.println(Arrays.toString(board.board[1]));
+        System.out.println(Arrays.toString(board.board[2]));
+        System.out.println(Arrays.toString(board.board[3]));
+        /*
+        |  _|  Q|  _|  _|
+        |  _|  _|  _|  Q|
+        |  Q|  _|  _|  _|
+        |  _|  _|  Q|  _|
+
+        [ 1, -1,  3,  1]
+        [ 3,  2,  2, -1]
+        [-1,  2,  2,  2]
+        [ 1,  3, -1,  1]
+         */
+        ChessBoard newBoard = new ChessBoard(4);
+        newBoard.run();
+        System.out.println(Arrays.toString(newBoard.board[0]));
+        System.out.println(Arrays.toString(newBoard.board[1]));
+        System.out.println(Arrays.toString(newBoard.board[2]));
+        System.out.println(Arrays.toString(newBoard.board[3]));
+
     }
 }
