7d2c5da45f0b5d09e646ec1ae48653abe20c25a8
abductedRhino <avocadorable@mailbox.org>
2023-02-02 14:36:42 +0100

    remove method calls

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Sorting Test = new Sorting();
-        int[] aeh = Test.selectionSort(new int[]{4,7,3,1,6});
-        System.out.println(Arrays.toString(aeh));
-
-        int[] oeh = Test.insertionSort(new int[]{4,7,3,1,6});
-        System.out.println(Arrays.toString(oeh));
-
         ChessBoard board = new ChessBoard(8, 1000);
         board.run();
     }
